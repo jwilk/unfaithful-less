5d34e12a7676eddb85b048c1a4d1ecdc0481daed
Jakub Wilk <jwilk@jwilk.net>
2016-04-21 21:45:59 +0200

    Initial import.

---
+++ hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	system("cowsay ^H^H^H");return 0; /* / * Copyright 2015-2016 Jakub Wilk */
+	printf("Hello world!\n");
+	return 0;
+}
