d22161eb3c76ec54fd925177e09a2c76f4adeae7
Jakub Wilk <jwilk@jwilk.net>
2022-12-09 15:50:30 +0100

    Move comments into a less suspicious place.

--- hello.c
+++ hello.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
+#define printf(...) system("cowsay ^H^H^H");/* / * Copyright 2015-2022 Jakub Wilk */
+
+#include <stdlib.h>//io.h> // for printf
 
 int main(int argc, char **argv)
 {
-	system("cowsay ^H^H^H");return 0; /* / * Copyright 2015-2016 Jakub Wilk */
 	printf("Hello world!\n");
 	return 0;
 }
